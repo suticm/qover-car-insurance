ce29b78f65044e5b00aac5cfdbe96bdc10b5faa8
Marko Sutic <m.sutic@vegait.rs>
2022-04-27 12:20:30 +0200

    [BE] added helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.use(helmet());
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
