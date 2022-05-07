31e56c808054a8a92de80eb0c3e4ff66537f2133
Marko Sutic <m.sutic@vegait.rs>
2022-05-07 15:56:40 +0200

    [BE] added database module

---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'process';
+import { DatabaseService } from './database.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('NODE_ENV') === 'test'
+            ? configService.get<string>('MONGO_TEST_CONNECTION_URL')
+            : configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [DatabaseService],
+  exports: [DatabaseService],
+})
+export class DatabaseModule {}
