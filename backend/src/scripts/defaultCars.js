84a6316957be6751a1fd643736f9dd6520aeba6a
Marko Sutic <m.sutic@vegait.rs>
2022-05-08 18:43:54 +0200

    [BE] exit script if db connection unsuccessful

--- backend/src/scripts/defaultCars.js
+++ backend/src/scripts/defaultCars.js
@@ -1,35 +1,41 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CarInsurance').then(() => {
-  console.log('inserting cars into db...');
-  mongoose.connection.db
-    .collection('cars')
-    .insertMany([
-      {
-        manufacturer: 'AUDI',
-        globalPrice: 250,
-        universalPercentageCoefficient: 0.3,
-        minAgeRestriction: 18,
-      },
-      {
-        manufacturer: 'BMW',
-        globalPrice: 150,
-        universalPercentageCoefficient: 0.4,
-        minAgeRestriction: 18,
-      },
-      {
-        manufacturer: 'PORSCHE',
-        globalPrice: 500,
-        universalPercentageCoefficient: 0.7,
-        minAgeRestriction: 25,
-      },
-    ])
-    .then(() => {
-      console.log('cars successfully inserted.');
-    })
-    .catch(() => {
-      console.log('failed to insert cars');
-    })
-    .finally(() => process.exit(0));
-});
+mongoose
+  .connect('mongodb://localhost:27017/CarInsurance')
+  .then(() => {
+    console.log('inserting cars into db...');
+    mongoose.connection.db
+      .collection('cars')
+      .insertMany([
+        {
+          manufacturer: 'AUDI',
+          globalPrice: 250,
+          universalPercentageCoefficient: 0.3,
+          minAgeRestriction: 18,
+        },
+        {
+          manufacturer: 'BMW',
+          globalPrice: 150,
+          universalPercentageCoefficient: 0.4,
+          minAgeRestriction: 18,
+        },
+        {
+          manufacturer: 'PORSCHE',
+          globalPrice: 500,
+          universalPercentageCoefficient: 0.7,
+          minAgeRestriction: 25,
+        },
+      ])
+      .then(() => {
+        console.log('cars successfully inserted.');
+      })
+      .catch(() => {
+        console.log('failed to insert cars');
+      })
+      .finally(() => process.exit(0));
+  })
+  .catch(() => {
+    console.log('mongoose connection failed');
+    process.exit(1);
+  });
